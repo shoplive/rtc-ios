562b5f11d70397a5f89367ea4f749dee3ff389d7
hassan0424 <hassan@shoplive.cloud>
2024-08-04 19:19:00 -0700

    framework loaded

---
+++ Frameworks/WebRTC.xcframework/ios-arm64/WebRTC.framework/Headers/RTCVideoCapturer.h
@@ -0,0 +1,45 @@
+/*
+ *  Copyright 2017 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#import <WebRTC/RTCVideoFrame.h>
+
+#import <WebRTC/RTCMacros.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class RTC_OBJC_TYPE(RTCVideoCapturer);
+
+RTC_OBJC_EXPORT @protocol RTC_OBJC_TYPE (RTCVideoCaptureShopliveDelegate)<NSObject>
+    - (void)capturer : (RTC_OBJC_TYPE(RTCVideoCapturer) *) capturer didCaptureVideoSampleBuffer: (CMSampleBufferRef) frame;
+    - (void)capturer : (RTC_OBJC_TYPE(RTCVideoCapturer) *) capturer didCaptureAudioFrame : (CMSampleBufferRef) frame;
+@end
+
+
+RTC_OBJC_EXPORT
+@protocol RTC_OBJC_TYPE
+(RTCVideoCapturerDelegate)<NSObject> 
+    - (void)capturer : (RTC_OBJC_TYPE(RTCVideoCapturer) *)capturer didCaptureVideoFrame: (RTC_OBJC_TYPE(RTCVideoFrame) *)frame;
+    
+@end
+
+
+
+RTC_OBJC_EXPORT
+@interface RTC_OBJC_TYPE (RTCVideoCapturer) : NSObject
+
+@property(nonatomic, weak) id<RTC_OBJC_TYPE(RTCVideoCapturerDelegate)> delegate;
+@property(nonatomic, weak) id<RTC_OBJC_TYPE(RTCVideoCaptureShopliveDelegate)> shopliveDelegate;
+
+
+- (instancetype)initWithDelegate:(id<RTC_OBJC_TYPE(RTCVideoCapturerDelegate)>)delegate shopliveDelegate: (id<RTC_OBJC_TYPE(RTCVideoCaptureShopliveDelegate)>)shopliveDelegate;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ShopLiveWebRTC.podspec
@@ -0,0 +1,19 @@
+Pod::Spec.new do |spec|
+  spec.name         = "ShopLiveWebRTC"
+  spec.version      = "1.0.0"
+  spec.summary      = "ShopLive SDK for iOS"
+
+  spec.homepage     = "http://shoplive.cloud"
+  spec.source   = { :git => 'https://github.com/shoplive/rtc-ios.git', :tag => spec.version }
+  spec.license = { :type => 'Copyright', :text => <<-LICENSE
+                 Copyright 2021
+                 Permission is granted to...
+                 LICENSE
+              }
+
+  spec.author             = { "Shoplive" => "shoplive-eng@shoplive.cloud" }
+  spec.platform     = :ios
+  spec.ios.deployment_target = '13.0'
+  spec.swift_version = "5"
+  spec.vendored_frameworks = 'Frameworks/WebRTC.xcframework'
+end
